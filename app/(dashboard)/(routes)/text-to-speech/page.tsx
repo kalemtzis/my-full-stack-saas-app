fa91bf8babfc5076064cb14ea9ceda262c69868e
kalemtzis <kalemtzis@csd.auth.gr>
2025-08-02 19:27:29 +0300

    init files

---
+++ app/(dashboard)/(routes)/text-to-speech/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/header'
+import React from 'react'
+
+const TextToSpeechPage = () => {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-rose-950 via-slate-950 to-purple-900 flex flex-col items-center justify-center p-3 gap-6'>
+      <Header title='Text To Speech' description='' />
+    </div>
+  )
+}
+
+export default TextToSpeechPage
